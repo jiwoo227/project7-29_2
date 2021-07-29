44fe090ae069fd894ca6b30596651fb9575e55ca
최지우 <w2040@e-mirim.hs.kr>
2021-07-29 11:49:51 +0900

    Edit activity_main.xml commit

--- app/src/main/java/com/example/project7_29_2/MainActivity.java
+++ app/src/main/java/com/example/project7_29_2/MainActivity.java
@@ -1,14 +1,106 @@
 package com.example.project7_29_2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextClock;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView list1;
+    TextView textMusic;
+    ProgressBar proBar;
+    ArrayList<String> arrayList;
+    String selectedMusic;
+    String musicPath = Environment.getExternalStoragePublicDirectory().getPath() + "/";
+    MediaPlayer media;
+    Button btnStart, btnStop;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("MP3 player");
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MODE_PRIVATE);
+        arrayList = new ArrayList<String>();
+        File[] listFiles = new File(musicPath).listFiles();
+        String fileName, extName;
+        for(File file : listFiles){
+            fileName = file.getName();
+            extName = fileName.substring(fileName.length()-3);
+            if(extName.equals("MP3"));
+                arrayList.add(fileName);
+        }
+        list1 = findViewById(R.id.list1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice);
+        list1.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        list1.setAdapter(adapter);
+        list1.setItemChecked(0, true);
+
+        list1.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                selectedMusic = arrayList.get(i);
+
+                return false;
+            }
+        });
+        selectedMusic = arrayList.get(0);
+
+        btnStart = findViewById(R.id.btn_start);
+        btnStop = findViewById(R.id.btn_stop);
+        textMusic = findViewById(R.id.text_music);
+        proBar = findViewById(R.id.progress);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    media = new MediaPlayer();
+                    media.setDataSource(musicPath + selectedMusic);
+                    media.prepare();
+                    media.start();
+                    btnStart.setClickable(false);
+                    btnStop.setClickable(true);
+                    textMusic.setText(selectedMusic + ":");
+                    proBar.setVisibility(View.VISIBLE);
+
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                media.stop();
+                media.reset();
+                btnStart.setClickable(true);
+                btnStop.setClickable(false);
+                textMusic.setText("실행중인 음악 중지");
+                proBar.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        btnStop.setClickable(false);
     }
 }
